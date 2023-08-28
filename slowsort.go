28e088e1c92b9326f577484943944b6424e0b118
macie <macie@users.noreply.github.com>
2023-08-28 11:01:12 +0200

    feat: Implement slowsort

---
+++ slowsort.go
@@ -0,0 +1,60 @@
+package sortof
+
+import (
+	"cmp"
+	"context"
+	"math"
+)
+
+// Slowsort sorts the slice x of any ordered type in ascending order. It is
+// practical implementation of multiply and surrender paradigm.
+//
+// When sorting floating-point numbers, NaNs are ordered before other values.
+// Cancelled context can leave slice partially ordered.
+//
+// According to algorithm authors, slowsort is most suitable for hourly rated
+// programmers.
+//
+// See: Andrei Broder and Jorge Stolfi. Pessimal Algorithms and Simplexity
+// Analysis. https://doi.org/10.1145/990534.990536
+func Slowsort[S ~[]E, E cmp.Ordered](ctx context.Context, x S) error {
+	return slowsort(ctx, x, 0, len(x)-1, cmp.Compare)
+}
+
+// SlowsortFunc sorts the slice x of any type in ascending order as
+// determined by the cmp function. Function cmp(a, b) should return a negative
+// number when a < b, a positive number when a > b and zero when a == b.
+//
+// Cancelled context can leave slice partially ordered.
+//
+// See: Andrei Broder and Jorge Stolfi. Pessimal Algorithms and Simplexity
+// Analysis. https://doi.org/10.1145/990534.990536
+func SlowsortFunc[S ~[]E, E any](ctx context.Context, x S, cmp func(a, b E) int) error {
+	return slowsort(ctx, x, 0, len(x)-1, cmp)
+}
+
+
+// slowsort sorts x[i:j].
+// The algorithm is based on multiple and surrender design with cancellation.
+// slowsort paper: https://doi.org/10.1145/990534.990536
+func slowsort[S ~[]E, E any](ctx context.Context, x S, i int, j int, cmp func(a, b E) int) error {
+	select {
+	case <-ctx.Done():
+		return context.Cause(ctx)
+	default:
+		if i >= j {
+			return nil
+		}
+
+		mid := int(math.Floor(float64((i + j) / 2)))
+		slowsort(ctx, x, i, mid, cmp)
+		slowsort(ctx, x, mid+1, j, cmp)
+		if cmp(x[j], x[mid]) == -1 {
+			x[mid], x[j] = x[j], x[mid]
+		}
+
+		slowsort(ctx, x, i, j-1, cmp)
+	}
+
+	return nil
+}
---
+++ slowsort_test.go
@@ -0,0 +1,73 @@
+package sortof
+
+import (
+	"context"
+	"math"
+	"slices"
+	"testing"
+)
+
+func TestSlowsortFloat(t *testing.T) {
+	ctx := context.Background()
+	testcases := [][]float64{
+		{1, 2, 3},
+		{math.MaxFloat64, 2, 0, -1, math.SmallestNonzeroFloat64, math.Log(-1)},
+	}
+	for _, tc := range testcases {
+		collection := slices.Clone(tc)
+
+		err := Slowsort(ctx, collection)
+		if err != nil {
+			t.Errorf("Slowsort(%v, %v) returns error: %v", ctx, tc, err)
+		}
+		if !slices.IsSorted(collection) {
+			want := slices.Clone(tc)
+			slices.Sort(want)
+			t.Errorf("Slowsort(%v, %v) cannot sort; got %v, want %v", ctx, tc, collection, want)
+		}
+	}
+}
+
+func TestSlowsortInt(t *testing.T) {
+	ctx := context.Background()
+	testcases := [][]int{
+		{1, 2, 3},
+		{math.MaxInt, 0, math.MinInt},
+	}
+	for _, tc := range testcases {
+		collection := slices.Clone(tc)
+
+		err := Slowsort(ctx, collection)
+		if err != nil {
+			t.Errorf("Slowsort(%v, %v) returns error: %v", ctx, tc, err)
+		}
+		if !slices.IsSorted(collection) {
+			want := slices.Clone(tc)
+			slices.Sort(want)
+			t.Errorf("Slowsort(%v, %v) cannot sort; got %v, want %v", ctx, tc, collection, want)
+		}
+	}
+}
+
+func TestSlowsortString(t *testing.T) {
+	ctx := context.Background()
+	testcases := [][]string{
+		{"1", "2", "3"},
+		{"100", "2", "0", "-1"},
+		{"1", "a", "."},
+		{"a", "", "b"},
+	}
+	for _, tc := range testcases {
+		collection := slices.Clone(tc)
+
+		err := Slowsort(ctx, collection)
+		if err != nil {
+			t.Errorf("Slowsort(%v, %v) returns error: %v", ctx, tc, err)
+		}
+		if !slices.IsSorted(collection) {
+			want := slices.Clone(tc)
+			slices.Sort(want)
+			t.Errorf("Slowsort(%v, %v) cannot sort; got %v, want %v", ctx, tc, collection, want)
+		}
+	}
+}
